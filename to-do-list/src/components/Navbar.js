85d1b328ab2d0a2a2d976b3e4a608c8b37bbb619
pollajoel <pollajoel2017@gmail.com>
2020-12-06 22:02:32 +0100

    separation de l'application en composants

---
+++ to-do-list/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {FaListAlt,FaCheckSquare,FaPlusSquare,FaTrash} from 'react-icons/fa';
+
+const Navbar = ()=>(
+<footer className="d-flex justify-content-between bg-secondary p-3" id="mainFooter">
+               <div className="btn-group">
+                 <a href="#" className="btn btn-outline-dark bg-light"><FaListAlt/></a>
+                 <a href="#" className="btn btn-outline-dark bg-light"><FaCheckSquare/></a>
+                 <a href="#" className="btn btn-outline-dark bg-light"><FaPlusSquare/></a>
+                 </div>
+                 <button className="btn btn-outline-dark bg-light"><FaTrash/></button>
+                 </footer>);
+
+export default Navbar
