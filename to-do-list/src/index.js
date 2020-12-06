1721759753440cde1ed6a25f62131032a5b26c43
pollajoel <pollajoel2017@gmail.com>
2020-12-06 21:47:18 +0100

    creation d'un component avec reactJs

--- to-do-list/src/index.js
+++ to-do-list/src/index.js
@@ -1,41 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import {FaListAlt,FaCheckSquare,FaPlusSquare,FaTrash} from 'react-icons/fa';
-
-class App extends React.Component{
-
-  render(){
-    return(
-      <section id="todo">
-        <h1 className="m-3">Liste de tâches</h1>
-        <ul className="list-group-item m-3">
-          <li className="list-group-item d-flex align-items-center">Ranger La vaisselle
-          <button className="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
-          </li>
-          <li className="list-group-item d-flex align-items-center">Repondre appels d'offres
-          <button className="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
-           </li>
-           <li className="list-group-item d-flex align-items-center">Signer contrat
-           <button className="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
-           </li>
-           <li className="list-group-item d-flex align-items-center">Ranger le salon
-           <button className="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
-           </li>
-           </ul>
-           <footer className="d-flex justify-content-between bg-secondary p-3" id="mainFooter">
-             <div className="btn-group">
-               <a href="#" className="btn btn-outline-dark bg-light"><FaListAlt/></a>
-               <a href="#" className="btn btn-outline-dark bg-light"><FaCheckSquare/></a>
-               <a href="#" className="btn btn-outline-dark bg-light"><FaPlusSquare/></a>
-               </div>
-               <button className="btn btn-outline-dark bg-light"><FaTrash/></button>
-               </footer>
-
-               </section>
-               );
-   }
-}
+import App from "./components/app";
 
 ReactDOM.render(<App/>,document.getElementById("root"));
-
+  
